e98d6097ddcb696c085abb7282cb8b0dc13239c1
Dimbay <dimasrismawan220803@gmail.com>
2026-07-03 18:30:55 +0700

    ganti pw

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         $email = 'kesiswaan@smpn1pamekasan.sch.id';
 
         // Password login Wakil Kesiswaan
-        $password = 'kesiswaan1';
+        $password = 'kesiswaan123';
 
         // =====================================================
         // HAPUS USER LAIN
